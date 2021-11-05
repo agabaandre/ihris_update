f67c140de7667b78b5d7aa0f6b5796d1a17aab33
Agaba Andrew <agabaandre@gmail.com>
2021-11-05 14:14:10 +0300

    http client

--- utils/HttpUtil.php
+++ utils/HttpUtil.php
@@ -11,104 +11,10 @@ class HttpUtil{
     public  function __construct(){
 
         $this->CI = & get_instance();
-        $this->client = new Client(['base_uri' => BIO_URL]);
+        //$this->client = new Client(['base_uri' => BIO_URL]);
         $this->ihrisclient = new Client(['base_uri' => iHRIS_URL]);
     }
 
-    public function sendRequest($endpoint = "",$method="",$headers = [],$body = []){
-        $request = new Request($method,$endpoint."/", $headers,json_encode($body));
-        $response =  $this->client->send($request);
-        return json_decode( (string) $response->getBody()->getContents());
-    }
-
-    public function sendiHRISRequest($endpoint = "",$method="",$headers = [],$body = []){
-       
-        $request = new Request($method,$endpoint."/", $headers,json_encode($body));
-        $response =  $this->ihrisclient->send($request);
-        return json_decode( (string) $response->getBody()->getContents());
-        //$result = json_decode($response->getBody()->getContents());
-    }
-   
-    public function getData($endpoint, $method="",$headers = [],$options)
-    {
-      
-       $url=BIO_URL.$endpoint;
-
-    
-
-        //do{
-            $response = $this->client->request(
-                $method,
-                $url,
-                [
-                    'headers' => $headers,
-                    'query' => [
-                        "page" => $options->page,
-                        "start_time"=>$options->start_time,
-                        "end_time"=>$options->end_time,
-                        "terminal_sn"=>$options->terminal_sn,
-                    ]
-                ]
-            );
-            
-    return json_decode( (string) $response->getBody()->getContents());
-    }
-    public function getTimeLogs($endpoint, $method="",$headers = [])
-    {
-        // die(var_dump($this->get_jwt_auth()));
-      
-       $url=BIO_URL.$endpoint;
-
-        //do{
-            $response = $this->client->request(
-                $method,
-                $url,
-                [
-                    'headers' => $headers
-                    
-                ]
-            );
-            
-       
-        return json_decode( (string) $response->getBody()->getContents());
-    }
-       public function get_List($endpoint, $method="",$headers = [],$options)
-    {
-        // die(var_dump($this->get_jwt_auth()));
-      
-       $url=BIO_URL.$endpoint;
-
-        //do{
-            $response = $this->client->request(
-                $method,
-                $url,
-                [
-                    'headers' => $headers,
-                    'query' => [
-                        
-                        "page" => $options->page
-                        ]
-                ]
-            );
-            
-       
-        return json_decode( (string) $response->getBody()->getContents());
-    }
-   
-
-    public function sendAsync($endpoint = "",$method="",$header = [],$body = []){
-        $headers  = ['Content-Type' => 'application/json'];
-        $headers  = array_merge($headers,$header);
-       // Send an asynchronous request.
-        $request  = new Request($method,$endpoint, $headers,json_encode($body));
-        $promise = $this->client->sendAsync($request)->then(function ($response) {
-            echo 'I completed! ' . $response->getBody();
-        });
-        $promise->wait();
-    return $promise;
-
-    }
-    //http requests
 
  public function curlsendHttpPost($endpoint,$headers,$body){
     $url=BIO_URL.$endpoint;
